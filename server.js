6ca93b59946e5bc7b05cf1c8ca6b8a2b8ec1eff0
Rahul dey <88264441+deyrahul885@users.noreply.github.com>
2022-08-24 12:29:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors')
+
+const app = express();
+
+// middle ware
+app.use(express.static('public')); //to access the files in public folder
+app.use(cors()); // it enables all cors requests
+app.use(fileUpload());
+
+
+app.get('/',(req,res)=>{
+    return res.status(200).send('Welcome to the server side')
+})
+
+// file upload api
+app.post('/upload', (req, res) => {
+
+    if (!req.files) {
+        return res.status(500).send({ msg: "file is not found" })
+    }
+        // accessing the file
+    const myFile = req.files.file;
+
+    //  mv() method places the file inside public directory
+    myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
+        if (err) {
+            console.log(err)
+            return res.status(500).send({ msg: "Error occured" });
+        }
+        // returing the response with file path and name
+        return res.send({name: myFile.name, path: `/${myFile.name}`});
+    });
+})
+
+
+app.listen(8000, () => {
+    console.log('server is running at PORT 8000');
+})
